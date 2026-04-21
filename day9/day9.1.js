d8f780292d9ff8bb5082e08230899b7f548640a3
Sai Yaswanth <yaswanth.pasupuleti@stackular.com>
2026-04-21 21:08:26 +0530

    day9.2

--- day9/day9.1.js
+++ day9/day9.1.js
@@ -1,13 +1,13 @@
-function bubbleSort(arr){
-for(let i=0;i<=arr.length-1;i++){
-var lowest=i
-for(let j=i+1;j<=arr.length-1;j++){
-if(arr[j]<arr[lowest]){
-lowest=j
+function bubbleSort(arr) {
+  for (let i = 0; i <= arr.length - 1; i++) {
+    var lowest = i;
+    for (let j = i + 1; j <= arr.length - 1; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    [arr[i], arr[lowest]] = [arr[lowest], arr[i]];
+  }
+  return arr;
 }
-}
-[arr[i],arr[lowest]]=[arr[lowest],arr[i]]
-}
-return arr;
-}
-console.log(bubbleSort([55,76,23,56,24]))
+console.log(bubbleSort([55, 76, 23, 56, 24]));
---
+++ day9/day9.2.js
@@ -0,0 +1,12 @@
+function selectionSort(arr) {
+  for (var i = 1; i <= arr.length - 1; i++) {
+    var currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+
+  return arr;
+}
+console.log(selectionSort([43, 65, 77, 89, 68]));
