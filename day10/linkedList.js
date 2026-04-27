0913766fa13a57d8d6a9557b8fb8eab63f0b7ab8
Sai Yaswanth <yaswanth.pasupuleti@stackular.com>
2026-04-27 18:55:15 +0530

    day10.2

--- day10/linkedList.js
+++ day10/linkedList.js
@@ -26,20 +26,54 @@ class SinglyLinkedList{
         this.length++
         return this
     }
+    // pop(val){
+    //     if(!this.head) return undefined;
+    //     var current= this.head;
+    //     var newTail= current;
+    //     while(current.next){
+    //         newTail=current;
+    //         current=current.next;
+            
+    //     }
+    //     this.tail=newTail;
+    //     this.tail.next=null;
+    //     this.length--;
+        
+        
+    // }
+
     pop(val){
         if(!this.head) return undefined;
-        var current= this.head;
-        var newTail= current;
-        while(current.next){
-            newTail=current;
-            current=current.next;
-            
+        var currentHead=this.head;
+        var newTail = currentHead;
+
+        while(currentHead.next){
+            newTail=currentHead;
+            currentHead=currentHead.next;
         }
         this.tail=newTail;
         this.tail.next=null;
         this.length--;
-        
-        
+    }
+
+    shift(){
+        if(!this.head) return undefined;
+        var currentHead=this.head;
+        this.head=currentHead.next;
+        this.length--;
+    }
+    unshift(val){
+        var currentVal=new Node(val);
+        if(!this.head){
+            this.head=currentVal;
+            this.tail= this.head;
+        }
+        else{
+            this.head=currentVal;
+            this.head.next=this.head;
+            this.length++
+        }
+        return this
     }
 }
 
@@ -49,5 +83,7 @@ var list=new SinglyLinkedList();
 list.push("hai")
 list.push("hello")
 list.push("wru")
-list.pop()
+// list.pop()
+// list.shift()
+list.unshift("I am from Unshift")
 console.log(list);
